fdb11914a9ab32f93fab655448abe1e4b5bc9f17
dev-dhruvipatel <dev.dhruvi2025@gmail.com>
2025-10-05 20:58:54 +0530

    Update EncryptionsPlugin.java

--- android/src/main/java/com/riguz/encryptions/EncryptionsPlugin.java
+++ android/src/main/java/com/riguz/encryptions/EncryptionsPlugin.java
@@ -3,16 +3,11 @@ package com.riguz.encryptions;
 import com.riguz.encryptions.invoker.AESCall;
 import com.riguz.encryptions.invoker.Argon2Call;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/*
-    https://github.com/flutter/flutter-intellij/issues/3153
-    Choose the menu item Flutter > Open Android module in Android Studio. That will open a new
-    window that will build the Android project with Gradle and support editing Java/Kotlin files.
- */
-
-public class EncryptionsPlugin {
+public class EncryptionsPlugin implements FlutterPlugin {
+    private MethodChannel channel;
     private static final FlutterCallExecutor executor = new FlutterCallExecutor();
 
     static {
@@ -20,8 +15,14 @@ public class EncryptionsPlugin {
         executor.register(Argon2Call.class);
     }
 
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "encryptions");
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "encryptions");
         channel.setMethodCallHandler(executor);
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
     }
 }
